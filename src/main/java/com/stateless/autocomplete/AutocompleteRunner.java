8b5153536a819474c71fb536a899c2400dfbc137
Abhijeet.Mohanty <mabhijeet1995@gmail.com>
2020-03-07 23:52:02 -0600

    Enhanced runner

--- src/main/java/com/stateless/autocomplete/AutocompleteRunner.java
+++ src/main/java/com/stateless/autocomplete/AutocompleteRunner.java
@@ -1,22 +1,56 @@
 package com.stateless.autocomplete;
 
+import com.stateless.autocomplete.autocompletion.ResultsGenerator;
+import com.stateless.autocomplete.executor.QueryExecutor;
 import com.stateless.autocomplete.loader.CorpusLoader;
 import com.stateless.autocomplete.utils.MessageUtils;
 import com.stateless.autocomplete.utils.MethodUtils;
+import com.sun.xml.internal.ws.util.StringUtils;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class AutocompleteRunner {
 
     public static void main(String[] args) {
+        run();
+    }
 
-        HashMap<String, Integer>[] corpusMapArray = CorpusLoader.createInstance().loadCorpusDataFromFile("corpus.txt");
+    private static void run() {
 
         printIntroductionMessage();
+        HashMap<String, Integer>[] corpusMapArray = CorpusLoader.createInstance().loadCorpusDataFromFile("corpus.txt");
+        ResultsGenerator resultsGenerator = ResultsGenerator.createInstance();
+        QueryExecutor queryExecutor = QueryExecutor.createInstance();
+        Scanner in = new Scanner(System.in);
+        String choice = "";
 
-        ResultsGenerator resultsGenerator = ResultsGenerator.createInstance(corpusMapArray);
+        do {
 
-        execute(resultsGenerator, "complete, bat, 2");
+            String query = in.nextLine();
+
+            if (canExecute(resultsGenerator, query)) {
+
+                String[] queryParts = query.split(",");
+
+                String action = queryParts[0].toLowerCase().trim();
+                String prefix = queryParts[1].toLowerCase().trim();
+                Integer maxCount = MethodUtils.getIntValue(queryParts[2].trim());
+
+                List<String> autocompletionStrings = queryExecutor.execute(resultsGenerator, corpusMapArray, action, prefix, maxCount);
+                autocompletionStrings.forEach(System.out::print);
+
+            } else {
+                System.out.println("Continuing the application");
+                continue;
+            }
+
+            System.out.println("Do you wish to continue (Y/N) ?");
+
+            choice = in.nextLine();
+
+        } while (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes"));
 
     }
 
@@ -29,15 +63,15 @@ public class AutocompleteRunner {
                 "|         This application allows the client to obtain possible autocomplete results for some query.            |\n" +
                 "|===============================================================================================================|\n" +
                 "|The client has the following options :                                                                         |\n" +
-                "|  1. Enter a search query in the form : complete,<prefix>,<max_count>                                          |\n" +
-                "|  2. Continue/Exit the application by entering Y or N through standard input                                   |\n" +
+                "|  1. Continue/Exit the application by entering Y or N through standard input                                   |\n" +
+                "|  2. If Y is entered, enter a search query in the form : complete,<prefix>,<max_count>                         |\n" +
                 "|===============================================================================================================|");
 
     }
 
-    private static void execute(ResultsGenerator resultsGenerator, String query) {
+    private static boolean canExecute(ResultsGenerator resultsGenerator, String query) {
 
-        resultsGenerator.processQuery(s -> {
+        return resultsGenerator.verifyQuery(s -> {
 
             String[] queryArray = s.split(",");
 
