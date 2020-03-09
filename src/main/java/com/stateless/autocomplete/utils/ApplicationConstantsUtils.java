26a81fa88f699af9796eb27921f894d8249bdda5
Abhijeet.Mohanty <mabhijeet1995@gmail.com>
2020-03-08 20:08:01 -0500

    Added utils file

---
+++ src/main/java/com/stateless/autocomplete/utils/ApplicationConstantsUtils.java
@@ -0,0 +1,8 @@
+package com.stateless.autocomplete.utils;
+
+public class ApplicationConstantsUtils {
+
+    public static final String RELATIVE_DEFAULT_CORPUS_PATH = "corpus-files/corpus.txt";
+
+    private ApplicationConstantsUtils() {}
+}
